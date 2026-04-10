8b7140886bfbd1b9b5f63363590c5fdf58083b72
Inversion-04 <ritiksaxena164@gmail.com>
2026-04-11 00:59:11 +0530

    fixed image handling and routes

--- Backend/Controllers/uploadController.js
+++ Backend/Controllers/uploadController.js
@@ -1,7 +1,7 @@
 const uploadImage = (req, res) => {
     res.json({
         success: 1,
-        image_url: `http://localhost:${process.env.PORT || 4000}/images/${req.file.filename}`
+        image: req.file.filename
     })
 }
 
